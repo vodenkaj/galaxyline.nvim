5a5fd3ed006fafc58f6f674105d95385dc68f800
vodenkaj <vodenkaj3@gmail.com>
2022-01-23 15:34:48 +0100

    fixed deprecated get_count call

--- lua/galaxyline/provider_diagnostic.lua
+++ lua/galaxyline/provider_diagnostic.lua
@@ -15,24 +15,17 @@ end
 -- see https://github.com/neovim/nvim-lspconfig
 local function get_nvim_lsp_diagnostic(diag_type)
   if next(lsp.buf_get_clients(0)) == nil then return '' end
-  local active_clients = lsp.get_active_clients()
 
-  if active_clients then
-    local count = 0
-
-    for _, client in ipairs(active_clients) do
-       count = count + lsp.diagnostic.get_count(api.nvim_get_current_buf(),diag_type,client.id)
-    end
-
-    if count ~= 0 then return count .. ' ' end
-  end
+  local count = #vim.diagnostic.get(api.nvim_get_current_buf(), { severity = vim.diagnostic.severity[diag_type]})
+  
+  if count ~= 0 then return count .. ' ' end
 end
 
 function M.get_diagnostic_error()
   if vim.fn.exists('*coc#rpc#start_server') == 1 then
     return get_coc_diagnostic('error')
   elseif not vim.tbl_isempty(lsp.buf_get_clients(0)) then
-    return get_nvim_lsp_diagnostic('Error')
+    return get_nvim_lsp_diagnostic('ERROR')
   end
   return ''
 end
@@ -41,7 +34,7 @@ function M.get_diagnostic_warn()
   if vim.fn.exists('*coc#rpc#start_server') == 1 then
     return get_coc_diagnostic('warning')
   elseif not vim.tbl_isempty(lsp.buf_get_clients(0)) then
-    return get_nvim_lsp_diagnostic('Warning')
+    return get_nvim_lsp_diagnostic('WARN')
   end
   return ''
 end
@@ -50,7 +43,7 @@ function M.get_diagnostic_hint()
   if vim.fn.exists('*coc#rpc#start_server') == 1 then
     return get_coc_diagnostic('hint')
   elseif not vim.tbl_isempty(lsp.buf_get_clients(0)) then
-    return get_nvim_lsp_diagnostic('Hint')
+    return get_nvim_lsp_diagnostic('HINT')
   end
   return ''
 end
@@ -59,7 +52,7 @@ function M.get_diagnostic_info()
   if vim.fn.exists('*coc#rpc#start_server') == 1 then
     return get_coc_diagnostic('information')
   elseif not vim.tbl_isempty(lsp.buf_get_clients(0)) then
-    return get_nvim_lsp_diagnostic('Information')
+    return get_nvim_lsp_diagnostic('INFO')
   end
   return ''
 end
